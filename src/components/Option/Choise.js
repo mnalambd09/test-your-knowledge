18bca64dbf89a3b25805286b3c178939b0f830d4
mnalambd09 <mnalambd09@gmail.com>
2022-10-16 10:26:07 +0600

    toast has been aded

--- src/components/Option/Choise.js
+++ src/components/Option/Choise.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Choise.css'
 import Form from 'react-bootstrap/Form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Option = ({multiple}) => {
-    // const {option} = multiple
-    // console.log(multiple)
+const Option = ({ multiple }) => {
+    const notify = () => toast("Your Answer is Wrong !!!");
     return (
         <div className='option'>
-            <p>Option : {multiple}</p>
+            <p>Option : {multiple} </p>
 
-            <Form.Check type="radio" aria-label="radio 1" />
-        
+            <Form.Check type="radio" aria-label="radio 1" onClick={notify} />
+            <ToastContainer />
+
         </div>
     );
 };
